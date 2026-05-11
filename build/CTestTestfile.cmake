e4b4ba1adadc34b578b11e2df391f9f977e4654d
Umrobotedge_juno01 <umrobotedgejuno@outlook.com>
2026-05-11 15:33:28 +0800

    Add the catkin_ws template to the code repository

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/src
+# Build directory: /home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/src:$ROS_PACKAGE_PATH"
