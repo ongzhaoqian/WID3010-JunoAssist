5984f8fe58848e84fca6c49b475c138bf2100c9e
Umrobotedge_juno01 <umrobotedgejuno@outlook.com>
2026-05-20 01:17:07 +0800

    Remove deprecated files and scripts related to catkin package setup
    - Deleted cmake.lock, env.sh, local_setup.bash, local_setup.sh, local_setup.zsh, setup.bash, setup.sh, and setup.zsh files as they are no longer needed.
    - Removed pkg-config files for language_pkg and perception_pkg, including their respective cmake configuration files.
    - Cleaned up the development environment by eliminating obsolete scripts and configuration files.

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/mustar/robot_project/devel;/home/mustar/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/mustar/robot_project/devel;/home/mustar/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- build/language_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/src/language_pkg
-# Build directory: /home/mustar/Desktop/WID3010-JunoAssist/WID3010-JunoAssist/build/language_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
